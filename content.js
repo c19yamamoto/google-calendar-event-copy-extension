d39d2a5312d1b8c0dab75fbb76a68e34ceff148c
Yamamoto Haruki <yamamotoharuki@team-lab.com>
2024-10-26 01:30:42 +0900

    :sparkles:feat: イベント上にボタンを配置

--- content.js
+++ content.js
@@ -1,19 +1,35 @@
-// ボタンを挿入したい部分のクラス名 (デフォルトではヘッダー部分)
-const headClass = ".DmDTHe";
-// イベント選択時に表示されるモーダルっぽいコンテンツのID
+// イベントモーダルのID
 const eventModalId = "xDetDlg";
 
-const buttonStyles = {
-  borderRadius: "10px",
-  margin: "0 10px",
-  backgroundColor: "#1A73E8",
-  color: "white",
-  cursor: "pointer",
-  border: "none",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  transition: "background-color 0.3s, color 0.3s",
+// 挿入するコンテナのクラス
+const headClass = ".pPTZAe";
+
+const svgNamespace = "http://www.w3.org/2000/svg";
+
+const createSvgElement = () => {
+  const svgWrapper = document.createElementNS(svgNamespace, "svg");
+  svgWrapper.setAttribute("xmlns", svgNamespace);
+  svgWrapper.setAttribute("viewBox", "0 0 640 512");
+  svgWrapper.setAttribute("width", "20");
+  svgWrapper.setAttribute("height", "20");
+  const path = document.createElementNS(svgNamespace, "path");
+  path.setAttribute(
+    "d",
+    "M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4-6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"
+  );
+
+  svgWrapper.addEventListener("click", () => {
+    const eventId = document.getElementById(eventModalId).dataset.eventid;
+    const eventUrl = "https://www.google.com/calendar/event?eid=" + eventId;
+    navigator.clipboard.writeText(eventUrl);
+    svgWrapper.style.animation = "clickAnimation 0.2s ease";
+    setTimeout(() => {
+      svgWrapper.style.animation = "";
+    }, 200);
+  });
+
+  svgWrapper.appendChild(path);
+  return svgWrapper;
 };
 
 const applyStyles = (element, styles) => {
@@ -22,50 +38,70 @@ const applyStyles = (element, styles) => {
   });
 };
 
-const createCopyButton = () => {
+const createSvgContainer = () => {
   const button = document.createElement("button");
-  button.id = "copy-event-id";
+  const buttonStyles = {
+    all: "unset",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+  };
   applyStyles(button, buttonStyles);
-  const buttonText = document.createElement("p");
-  buttonText.textContent = "Copy URL";
-  button.appendChild(buttonText);
-  addCopyEventListener(button);
+  const svgElement = createSvgElement();
+  button.appendChild(svgElement);
   return button;
 };
 
-const addCopyEventListener = (button) => {
-  button.addEventListener("click", () => {
-    const eventElement = document.getElementById(eventModalId);
-    if (!eventElement) {
-      window.alert("コピーしたいイベントを選択してください");
-      return;
+const observerCallback = (mutationsList) => {
+  let shouldReAddListener = false;
+
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      const eventElement = document.getElementById(eventModalId);
+      if (eventElement) {
+        shouldReAddListener = true;
+        break;
+      }
     }
-    const eventId = eventElement.dataset.eventid;
-    const eventUrl = "https://www.google.com/calendar/event?eid=" + eventId;
-    navigator.clipboard
-      .writeText(eventUrl)
-      .then(() => {
-        // ボタンの色変更
-        applyStyles(button, { backgroundColor: "green", color: "white" });
-        button.querySelector("p").textContent = "Copied!";
+  }
 
-        setTimeout(() => {
-          applyStyles(button, buttonStyles);
-          button.querySelector("p").textContent = "Copy URL";
-        }, 2000);
-      })
-      .catch((err) => {
-        window.alert("Error: " + err);
-      });
-  });
-};
-
-const init = () => {
-  const container = document.querySelector(headClass);
-  if (container) {
-    const button = createCopyButton();
-    container.appendChild(button);
+  if (shouldReAddListener) {
+    const eventElement = document.getElementById(eventModalId);
+    if (eventElement) {
+      const container = document.querySelector(headClass);
+      if (container && !container.querySelector("#copy-event-svg")) {
+        const svgContainer = createSvgContainer();
+        svgContainer.id = "copy-event-svg";
+        if (container.children.length > 1) {
+          container.insertBefore(
+            svgContainer,
+            container.children[container.children.length - 1]
+          );
+        } else {
+          container.appendChild(svgContainer);
+        }
+      }
+    }
   }
 };
 
-init();
+const initObserver = () => {
+  const observer = new MutationObserver(observerCallback);
+  observer.observe(document.body, { childList: true, subtree: true });
+};
+
+const style = document.createElement("style");
+style.innerHTML = `
+@keyframes clickAnimation {
+  0% { transform: scale(1); }
+  50% { transform: scale(0.8); }
+  100% { transform: scale(1); }
+}
+#copy-event-svg svg {
+  transition: transform 0.2s ease;
+}
+`;
+document.head.appendChild(style);
+
+initObserver();
