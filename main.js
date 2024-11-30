69922640d7430451be0f985c76e8bab799c271c9
c19yamahar <c19yamamoto@gmail.com>
2024-11-30 17:11:42 +0900

    :recycle:refactor: リファクタなど

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
+// イベントモーダルのID
 const eventModalId = "xDetDlg";
+
+// 挿入するコンテナのクラス
 const headClass = ".pPTZAe";
+
+// タイトル情報を持つクラス
 const titleClass = ".UfeRlc";
+
 const svgNamespace = "http://www.w3.org/2000/svg";
 
-// SVG要素を作成する関数
 const createSvgElement = () => {
+  // font-awesome の link アイコンを使用
   const svgWrapper = document.createElementNS(svgNamespace, "svg");
   svgWrapper.setAttribute("xmlns", svgNamespace);
   svgWrapper.setAttribute("viewBox", "0 0 640 512");
   svgWrapper.setAttribute("width", "20");
   svgWrapper.setAttribute("height", "20");
-
   const path = document.createElementNS(svgNamespace, "path");
   path.setAttribute(
     "d",
     "M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4-6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"
   );
+
   svgWrapper.appendChild(path);
   return svgWrapper;
 };
 
-// スタイルを適用する関数
 const applyStyles = (element, styles) => {
   Object.entries(styles).forEach(([key, value]) => {
     element.style[key] = value;
   });
 };
 
-// クリックアニメーションを実行する関数
 const animateSvgClick = (svgElement) => {
   svgElement.style.animation = "clickAnimation 0.2s ease";
   setTimeout(() => {
@@ -35,7 +39,6 @@ const animateSvgClick = (svgElement) => {
   }, 200);
 };
 
-// イベントURLをクリップボードにコピーする関数
 const copyEventUrlToClipboard = () => {
   const eventId = document.getElementById(eventModalId).dataset.eventid;
   const eventUrl = `https://www.google.com/calendar/event?eid=${eventId}`;
@@ -50,7 +53,6 @@ const copyEventUrlToClipboard = () => {
   });
 };
 
-// SVGコンテナを作成する関数
 const createSvgContainer = () => {
   const button = document.createElement("button");
   applyStyles(button, {
@@ -74,7 +76,6 @@ const createSvgContainer = () => {
   return button;
 };
 
-// モーダルが変更された時にSVGボタンを再追加するための関数
 const observerCallback = (mutationsList) => {
   let shouldReAddListener = false;
 
@@ -93,6 +94,7 @@ const observerCallback = (mutationsList) => {
     if (container && !container.querySelector("#copy-event-svg")) {
       const svgContainer = createSvgContainer();
       svgContainer.id = "copy-event-svg";
+      // コンテナの最後から2番目に挿入
       container.insertBefore(
         svgContainer,
         container.children[container.children.length - 1]
@@ -101,13 +103,11 @@ const observerCallback = (mutationsList) => {
   }
 };
 
-// MutationObserverを初期化する関数
 const initObserver = () => {
   const observer = new MutationObserver(observerCallback);
   observer.observe(document.body, { childList: true, subtree: true });
 };
 
-// スタイルを追加
 const style = document.createElement("style");
 style.innerHTML = `
 @keyframes clickAnimation {
@@ -121,5 +121,4 @@ style.innerHTML = `
 `;
 document.head.appendChild(style);
 
-// MutationObserverを開始
 initObserver();
--- options/options.js
+++ options/options.js
@@ -1,10 +1,8 @@
-// Saves options to chrome.storage
 const saveOptions = () => {
   const iswithTitleEnabled =
     document.getElementById("with-title-toggle").checked;
 
   chrome.storage.sync.set({ withTitleEnabled: iswithTitleEnabled }, () => {
-    // Update status to let user know options were saved.
     const status = document.getElementById("status");
     status.textContent = "Options saved.";
     setTimeout(() => {
@@ -13,7 +11,6 @@ const saveOptions = () => {
   });
 };
 
-// Restores checkbox state using the preferences stored in chrome.storage.
 const restoreOptions = () => {
   chrome.storage.sync.get({ withTitleEnabled: false }, (items) => {
     document.getElementById("with-title-toggle").checked =
