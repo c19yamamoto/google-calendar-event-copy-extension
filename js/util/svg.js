619b7fb728a02a95e80fd99db581f2e19d7ac7a6
c19yamahar <c19yamamoto@gmail.com>
2025-05-10 16:04:41 +0900

    :sparkles:feat: アニメーションの変更

--- js/util/svg.js
+++ js/util/svg.js
@@ -6,15 +6,23 @@
  * リンクアイコンのSVG要素を作成
  */
 const createSvgElement = () => {
-  // object要素を作成してSVGファイルを読み込み
-  const obj = document.createElement('object');
-  obj.data = chrome.runtime.getURL('assets/images/link-icon.svg');
-  obj.type = 'image/svg+xml';
-  obj.width = 20;
-  obj.height = 20;
-  obj.classList.add('svg-icon');
-  
-  return obj;
+  // SVG要素を直接作成
+  const svgContainer = document.createElement("div");
+  svgContainer.classList.add("svg-icon");
+  svgContainer.style.width = "20px";
+  svgContainer.style.height = "20px";
+
+  // NOTE: svg ファイルに切り出したらスタイルが上手く適用されなくなった
+  svgContainer.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="20" height="20">
+      <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4-6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+    </svg>
+  `;
+
+  // クリックイベントを親要素に伝播させる
+  svgContainer.style.pointerEvents = "none";
+
+  return svgContainer;
 };
 
 /**
@@ -22,11 +30,11 @@ const createSvgElement = () => {
  */
 const animateSvgClick = element => {
   // クリックアニメーションを適用
-  element.style.animation = 'clickAnimation 0.2s ease';
+  element.style.animation = "clickAnimation 0.2s ease";
 
   // アニメーション終了後にリセット
   setTimeout(() => {
-    element.style.animation = '';
+    element.style.animation = "";
   }, 200);
 };
 
@@ -35,12 +43,12 @@ const animateSvgClick = element => {
  */
 const createSvgContainer = onClickHandler => {
   // ボタン要素を作成
-  const button = document.createElement('button');
+  const button = document.createElement("button");
   button.id = DOM_SELECTORS.COPY_BUTTON_ID;
 
   // SVG要素を作成してボタンに追加
   const svgElement = createSvgElement();
-  button.addEventListener('click', () => onClickHandler(svgElement));
+  button.addEventListener("click", () => onClickHandler(svgElement));
   button.appendChild(svgElement);
 
   return button;
